456de2c0a9a7a85c63dcda91e1bd670b5a86dd18
Avarelaf <agustin.varela@campusdual.com>
2022-10-17 11:52:14 +0200

    esquema de landing page

--- demo-frontend/src/app/app-routing.module.ts
+++ demo-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { LandingComponent } from './core/landing/landing.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent,  pathMatch: 'full'},
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  /*{ path: '', redirectTo: 'login', pathMatch: 'full' },*/
+  { path: '', component:LandingComponent},
   { path: 'contacts', loadChildren: () => import('./main/contacts/contacts.module').then(x => x.ContactsModule) },
   { path: 'users', loadChildren: () => import('./main/users/users.module').then(x => x.UsersModule)},
 ];
--- demo-frontend/src/app/auth/auth-routing.module.ts
+++ demo-frontend/src/app/auth/auth-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from '../core/landing/landing.component';
 import { CreateComponent } from '../main/users/create/create.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'users/createUser', component: CreateComponent},
-  { path: '', redirectTo: 'login', pathMatch: 'full'}
+  /*{ path: '', redirectTo: 'login, pathMatch: 'full'}*/
+  { path:'', component:LandingComponent }
 ];
 
 @NgModule({
