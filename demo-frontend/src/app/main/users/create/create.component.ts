8c418208a14ac5ecf3fcd2bd0c286e07cd6c1d24
javierotso <85392399+javierotso@users.noreply.github.com>
2022-10-19 09:09:44 +0200

    Change alerts at createComponent

--- demo-frontend/src/app/main/users/create/create.component.ts
+++ demo-frontend/src/app/main/users/create/create.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/model/user';
 import { UserService } from 'src/app/services/user.service';
-
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Profiles } from 'src/app/model/profile';
-import { throwToolbarMixedModesError } from '@angular/material/toolbar';
-
-
+import swal from "sweetalert2";
+import { TranslateService } from '@ngx-translate/core';
 
 interface Tipo {
   value: number,
@@ -20,27 +18,30 @@ interface Tipo {
 })
 export class CreateComponent implements OnInit {
 
-  user : User;
+  user: User;
   userForm: FormGroup;
-  selected: number = 1;
-  nome :string="nombre";
-  description: string ="shshshs";
+  selected= 1;
+  nome: string = "nombre";
+  description: string = "shshshs";
+  errors: string[];
 
-    
+
 
   categories: Tipo[] = [
-    {value: 2, viewValue: 'Productor'},
-    {value: 3, viewValue: 'Cliente'},
+    { value: 2, viewValue: 'Productor' },
+    { value: 3, viewValue: 'Cliente' },
   ];
 
-  constructor(    
-    private userService : UserService,
+  constructor(
+    private userService: UserService,
     private fb: FormBuilder,
-    private router: Router
-    ) { 
-      this.user = new User();
-      this.user.profile = new Profiles();
-    }
+
+    private router: Router,
+    private translate: TranslateService,
+  ) {
+    this.user = new User();
+    this.user.profile = new Profiles();
+  }
 
   ngOnInit(): void {
     this.user.profile.id = this.selected;
@@ -49,34 +50,51 @@ export class CreateComponent implements OnInit {
     this.createFormGroup();
   }
 
-
-
-  createFormGroup(){
-    this.userForm = this.fb.group({      
+  createFormGroup() {
+    this.userForm = this.fb.group({
       email: [this.user.email, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")],
       login: [this.user.login],
-      password:[this.user.password],
-      profile:[ this.user.profile]
+      password: [this.user.password],
+      profile: [this.user.profile]
       /*profile:[ {id: this.selected, name: this.nome, description:this.description}]*/
+
     });
   }
-  
+
   cancel() {
     this.router.navigate(['/login']);
   }
 
   save() {
     const newUser: User = Object.assign({}, this.userForm.value);
+    let message;
     this.userService.createUser(newUser).subscribe((response) => {
       console.log(this);
+      message = this.translate.instant("USER_CREATE_SUCCESS")
+      swal.fire(message, "", 'success');
       this.redirectList(response);
-    });
+    }, err => {
+      console.log(err.message);
+
+      if (err.error.errors.toString().includes("users_login_unique")) {
+        message = this.translate.instant("USER_LOGIN_UNIQUE");
+      } else if (err.error.errors.toString().includes("users_email_unique")) {
+        message = this.translate.instant("USER_EMAIL_UNIQUE");
+      }
+      swal.fire({
+        confirmButtonColor: '#bfedff',
+        title: this.translate.instant('ERROR'),
+        text: this.translate.instant(message),
+        icon: 'error'
+      });
+    }
+    );
   }
 
   redirectList(response: any) {
     if (response.responseCode === 'OK') {
       this.router.navigate(['/login']);
-    }else{
+    } else {
       console.log(response);
     }
   }
