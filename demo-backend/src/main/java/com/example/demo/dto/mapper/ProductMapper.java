7c759d2f364045a1a10479a5148e68eaec6b2393
Avarelaf <agustin.varela@campusdual.com>
2022-10-20 10:24:27 +0200

    Añadido productmapper

---
+++ demo-backend/src/main/java/com/example/demo/dto/mapper/ProductMapper.java
@@ -0,0 +1,20 @@
+package com.example.demo.dto.mapper;
+
+import java.util.List;
+
+import org.mapstruct.factory.Mappers;
+
+import com.example.demo.dto.ProductDTO;
+import com.example.demo.entity.Product;
+
+public interface ProductMapper {
+	
+    ProductMapper INSTANCE = Mappers.getMapper( ProductMapper.class );
+    
+    ProductDTO productToProductDto(Product product);
+    
+    List<ProductDTO> productToProductDtoList(List<Product> products);
+    
+    Product contactDTOtoContact(ProductDTO productdto);
+
+}
