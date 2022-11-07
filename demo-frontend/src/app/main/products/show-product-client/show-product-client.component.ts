5ce6db5400d09ee642b947a72e37fb8057e45412
javierotso <85392399+javierotso@users.noreply.github.com>
2022-11-07 10:06:52 +0100

    Added pagination on product-client view

--- demo-frontend/src/app/main/products/show-product-client/show-product-client.component.ts
+++ demo-frontend/src/app/main/products/show-product-client/show-product-client.component.ts
@@ -1,9 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { fromEvent } from 'rxjs';
 import { ShowProductDatasource } from 'src/app/model/datasource/showproduct.datasource';
-import { AnyField, AnyPageFilter } from 'src/app/model/rest/filter';
+import { Product } from 'src/app/model/product';
+import { AnyField, AnyPageFilter, SortFilter } from 'src/app/model/rest/filter';
 import { ProductService } from 'src/app/services/product.service';
+import { SelectionModel } from '@angular/cdk/collections';
+import { ElementRef, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { debounceTime, distinctUntilChanged, merge, Observable, Observer, tap } from 'rxjs';
+import { User } from 'src/app/model/user';
+import { ConfirmationDialogComponent } from 'src/app/shared/confirmation-dialog/confirmation-dialog.component';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-show-product-client',
@@ -14,8 +25,12 @@ export class ShowProductClientComponent implements OnInit {
 
   dataSource: ShowProductDatasource;
   fields = ['name', 'quantity', 'typeProd', 'price', 'user.login'];
-  products;
+  products: Product[];
+  selection = new SelectionModel<Product>(true, []);
+  error = false;
 
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   constructor(
     private productService: ProductService,
     private translate: TranslateService,
@@ -28,7 +43,7 @@ export class ShowProductClientComponent implements OnInit {
       '',
       this.fields.map((field) => new AnyField(field)),
       0,
-      16,
+      10,
       'name'
     );
     this.dataSource.getProducts(pageFilter);
@@ -37,7 +52,31 @@ export class ShowProductClientComponent implements OnInit {
     });
   }
 
+  ngAfterViewInit(): void {
+    this.paginator.page.pipe(
+        tap(() => {
+          this.loadProductsPage();
+        })
+      )
+      .subscribe()
+  }
+
   contact(email: string, productName: string) {
     location.href = "mailto:" + email + "?Subject=Reserva " + productName + "&body=Hola!%0AMe%20gustaría%20reservar%20este%20producto:%0A-%20Producto: " + productName + "%0A-%20Cantidad: 1";
+  }
+
+  loadProductsPage() {
+    this.selection.clear();
+    this.error = false;
+    const pageFilter = new AnyPageFilter(
+      '',
+      this.fields.map((field) => new AnyField(field)),
+      this.paginator.pageIndex,
+      this.paginator.pageSize
+    );
+    this.dataSource.getProducts(pageFilter);
+    this.productService.getProducts(pageFilter).subscribe((a) => {
+      this.products = a.data;
+    });
   }
 }
